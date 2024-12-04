864d8ed92ae20aef1b313470cb520bb4daef85ac
Kristian Brudeli <krbrudeli@gmail.com>
2024-12-04 17:39:04 +0100

    Part 1 done

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,4 +1,4 @@
-use std::borrow::BorrowMut;
+use std::collections::HashMap;
 
 use itertools::Itertools;
 use lazy_static::lazy_static;
@@ -44,17 +44,49 @@ pub fn part_one(input: &str) -> Option<i64> {
     Some(total_diff)
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
+fn counts(numbers: Vec<i64>) -> HashMap<i64, i64> {
+    let mut hashmap: HashMap<i64, i64> = HashMap::new();
+
+    for number in &numbers {
+        hashmap.entry(*number).and_modify(|entry| *entry += 1).or_insert(1);
+    }
+
+    hashmap
+}
+
+pub fn part_two(input: &str) -> Option<i64> {
     let tuples = input.lines().map(Line::parse).collect::<Option<Vec<_>>>()?;
 
-    let right_occurences = tuples.map(|l| l.right)
+    let right: Vec<i64> = tuples.iter().map(|l| l.right).collect();
+    let right_counts = counts(right);
+
+    let left: Vec<i64> = tuples.iter().map(|l| l.left).collect();
+
+    let similarity: i64 = left
+        .into_iter()
+        .map(|num| {
+            match right_counts.get(&num) {
+                Some(&i) => num * i,
+                None => 0
+            }
+        }).sum();
     
-    None
+    Some(similarity)
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_counts() {
+        let v: Vec<i64> = vec![3, 4, 3];
+        let counts = counts(v);
+
+        assert_eq!(Some(&2), counts.get(&3));
+        assert_eq!(Some(&1), counts.get(&4));
+        assert_eq!(None, counts.get(&1));
+    }
 
     #[test]
     fn test_part_one() {
