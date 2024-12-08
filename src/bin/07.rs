aa9f2385c8048c3cba3ab90949d9b0b76ecfc31a
Kristian Brudeli <krbrudeli@gmail.com>
2024-12-08 21:08:46 +0100

    WIP

--- src/bin/07.rs
+++ src/bin/07.rs
@@ -4,15 +4,15 @@ advent_of_code::solution!(7);
 
 #[derive(Debug, PartialEq)]
 struct CalibrationEquation {
-    test_value: u32,
-    values: Vec<u32>,
+    test_value: u64,
+    values: Vec<u64>,
 }
 
 impl CalibrationEquation {
     fn parse(line: &str) -> Option<CalibrationEquation> {
         let (left, right) = line.split_once(": ")?;
-        let test_value: u32 = left.parse().ok()?;
-        let values: Vec<u32> = right
+        let test_value: u64 = left.parse().ok()?;
+        let values: Vec<u64> = right
             .split(" ")
             .map(|v| v.parse().ok())
             .collect::<Option<Vec<_>>>()?;
@@ -21,7 +21,7 @@ impl CalibrationEquation {
     }
 }
 
-fn calibration_result(lhs: Option<u32>, rest: Vec<u32>, test_value: u32) -> u32 {
+fn calibration_result_part_1(lhs: Option<u64>, rest: Vec<u64>, test_value: u64) -> u64 {
     if rest.len() == 0 {
         // Test if rest equals test_value
         if lhs.expect("lhs must not be empty") == test_value {
@@ -34,35 +34,116 @@ fn calibration_result(lhs: Option<u32>, rest: Vec<u32>, test_value: u32) -> u32 
     if let Some(lhs_) = lhs {
         let next = rest.get(0).expect("rest must not be empty").to_owned();
         let next_rest = rest.get(1..).map_or(vec![], |x| x.to_vec());
-    
+
         max(
-            calibration_result(Some(lhs_ + next), next_rest.clone(), test_value),
-            calibration_result(Some(lhs_ * next), next_rest, test_value),
+            calibration_result_part_1(Some(lhs_ + next), next_rest.clone(), test_value),
+            calibration_result_part_1(Some(lhs_ * next), next_rest, test_value),
         )
     } else {
         let next = rest.get(0).expect("rest must not be empty").to_owned();
         let next_rest = rest.get(1..).map_or(vec![], |x| x.to_vec());
-        
-        calibration_result(Some(next), next_rest, test_value)
+
+        calibration_result_part_1(Some(next), next_rest, test_value)
     }
 }
 
-pub fn part_one(input: &str) -> Option<u32> {
+fn concatenate(left: u64, right: u64) -> u64 {
+    let right_digits = right.checked_ilog10().unwrap_or(0) + 1;
+    (left * 10u64.pow(right_digits)) + right
+}
+
+fn concatenate_first_two(lhs: u64, rest: Vec<u64>) -> Vec<u64> {
+    match rest.len() {
+        0 => rest,
+        1 => vec![concatenate(lhs, rest[0])],
+        _ => {
+            let mut beginning = vec![concatenate(lhs, rest[0])];
+            let mut rest = rest.get(1..).unwrap().to_vec();
+            beginning.append(&mut rest);
+            beginning
+        }
+    }
+}
+
+fn calibration_result_part_2(
+    lhs: Option<u64>,
+    rest: Vec<u64>,
+    test_value: u64,
+    has_concatenated: bool,
+) -> u64 {
+    if rest.len() == 0 {
+        // Test if rest equals test_value
+        if lhs.expect("lhs must not be empty") == test_value {
+            return test_value;
+        } else {
+            return 0;
+        }
+    }
+
+    let concatenate_variant: u64 = if !has_concatenated {
+        let concatenated_rest = concatenate_first_two(rest.clone());
+        calibration_result_part_2(lhs, concatenated_rest, test_value, true)
+    } else {
+        0
+    };
+
+    max(
+        if let Some(lhs_) = lhs {
+            let next = rest.get(0).expect("rest must not be empty").to_owned();
+            let next_rest = rest.get(1..).map_or(vec![], |x| x.to_vec());
+
+            max(
+                calibration_result_part_2(
+                    Some(lhs_ + next),
+                    next_rest.clone(),
+                    test_value,
+                    has_concatenated,
+                ),
+                calibration_result_part_2(
+                    Some(lhs_ * next),
+                    next_rest,
+                    test_value,
+                    has_concatenated,
+                ),
+            )
+        } else {
+            let next = rest.get(0).expect("rest must not be empty").to_owned();
+            let next_rest = rest.get(1..).map_or(vec![], |x| x.to_vec());
+
+            calibration_result_part_2(Some(next), next_rest, test_value, has_concatenated)
+        },
+        concatenate_variant,
+    )
+}
+
+pub fn part_one(input: &str) -> Option<u64> {
     let calibration_eqs = input
         .lines()
         .map(CalibrationEquation::parse)
-        .collect::<Option<Vec<_>>>().expect("should parse");
+        .collect::<Option<Vec<_>>>()
+        .expect("should parse");
 
-    let total_calibration_result: u32 = calibration_eqs
+    let total_calibration_result: u64 = calibration_eqs
         .iter()
-        .map(|eq| calibration_result(None, eq.values.clone(), eq.test_value))
+        .map(|eq| calibration_result_part_1(None, eq.values.clone(), eq.test_value))
         .sum();
 
     Some(total_calibration_result)
 }
 
-pub fn part_two(_input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<u64> {
+    let calibration_eqs = input
+        .lines()
+        .map(CalibrationEquation::parse)
+        .collect::<Option<Vec<_>>>()
+        .expect("should parse");
+
+    let total_calibration_result: u64 = calibration_eqs
+        .iter()
+        .map(|eq| calibration_result_part_2(None, eq.values.clone(), eq.test_value, false))
+        .sum();
+
+    Some(total_calibration_result)
 }
 
 #[cfg(test)]
@@ -81,6 +162,20 @@ mod tests {
     }
 
     #[test]
+    fn test_concatenate() {
+        assert_eq!(156, concatenate(15, 6));
+        assert_eq!(1560, concatenate(15, 60));
+    }
+
+    #[test]
+    fn test_concatenate_first_two() {
+        assert_eq!(vec![315, 6], concatenate_first_two(3, vec![15, 6]));
+        assert_eq!(vec![3156], concatenate_first_two(3, vec![156]));
+        assert_eq!(vec![3], concatenate_first_two(3, vec![]));
+        assert_eq!(vec![315, 6, 4], concatenate_first_two(3, vec![15, 6, 4]));
+    }
+
+    #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
         assert_eq!(result, Some(3749));
@@ -89,6 +184,13 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(11387));
+    }
+
+    #[test]
+    fn test_part_two_156() {
+        // assert_eq!(Some(156), part_two("156: 15 6"));
+        assert_eq!(Some(7290), part_two("7290: 6 8 6 15"));
+        // assert_eq!(Some(192), part_two("192: 17 8 14"));
     }
 }
