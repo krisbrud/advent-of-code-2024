074b08ca5744ad8cc73d838ce100725b9825c3a5
Kristian Brudeli <krbrudeli@gmail.com>
2024-12-12 00:03:56 +0100

    WIP day 6

--- src/bin/06.rs
+++ src/bin/06.rs
@@ -1,5 +1,5 @@
 use itertools::Itertools;
-use std::collections::HashMap;
+use std::{collections::HashMap, env::var};
 
 advent_of_code::solution!(6);
 
@@ -133,13 +133,18 @@ impl Board {
                     Tile::Unvisited => '.',
                     Tile::Obstruction => '#',
                     Tile::Guard(direction) => match direction {
+                        Direction::North => 'N',
+                        Direction::East => 'E',
+                        Direction::South => 'S',
+                        Direction::West => 'W',
+                    },
+                    Tile::VisitedPart1 => 'X',
+                    Tile::VisitedPart2(direction) => match direction {
                         Direction::North => '^',
                         Direction::East => '>',
                         Direction::South => 'V',
                         Direction::West => '<',
                     },
-                    Tile::VisitedPart1 => 'X',
-                    Tile::VisitedPart2(_) => 'Q',
                 };
                 print!("{}", char);
             }
@@ -196,7 +201,64 @@ fn step_part_1(tiles: &mut HashMap<(usize, usize), Tile>, size: &BoardSize) -> b
     }
 }
 
-fn loops(tiles: &HashMap<(usize, usize), Tile>, size: &BoardSize, has_branched: bool) -> u32 {
+// fn loops(tiles: &mut HashMap<(usize, usize), Tile>, size: &BoardSize) -> u32 {
+//     // let (guard_pos, guard_tile) = tiles.iter().find(|(pos, tile)| {
+//     let (guard_pos, guard_tile) = tiles
+//         .iter()
+//         .find(|(_, tile)| matches!(tile, Tile::Guard(_)))
+//         .map(|(pos, tile)| (*pos, tile.clone()))
+//         .unwrap();
+
+//     // tiles.insert(guard_pos, Tile::VisitedPart2());
+
+//     let guard_direction = match guard_tile.clone() {
+//         Tile::Guard(direction) => direction,
+//         _ => panic!("Guard tile should always be guard"),
+//     };
+
+//     // let non_branching_loops: u32 = if let Some(pos_in_front) =
+//     if let Some(pos_in_front) = pos_in_direction(guard_pos, &guard_direction, size) {
+//         tiles.insert(guard_pos, Tile::VisitedPart2(guard_direction.clone()));
+//         if let Some(tile_in_front) = tiles.get(&pos_in_front) {
+//             match tile_in_front {
+//                 Tile::Unvisited => {
+//                     // Set tile in front as guard with same direction
+//                     tiles.insert(pos_in_front, guard_tile);
+//                     loops(tiles, size)
+//                 }
+//                 Tile::VisitedPart2(prev_direction) => {
+//                     if guard_direction == *prev_direction {
+//                         // Found a loop!
+//                         return 1;
+//                     }
+//                     tiles.insert(pos_in_front, guard_tile);
+//                     loops(tiles, size)
+//                 }
+//                 Tile::Obstruction => {
+//                     let turned_direction = guard_direction.turn_right();
+//                     if let Some(turned_pos) = pos_in_direction(guard_pos, &turned_direction, &size)
+//                     {
+//                         tiles.insert(turned_pos, Tile::Guard(turned_direction.clone()));
+//                         loops(tiles, size)
+//                     } else {
+//                         // Outside of board
+//                         0
+//                     }
+//                 }
+//                 Tile::Guard(_) => {
+//                     panic!("should not have more than one guard")
+//                 }
+//                 Tile::VisitedPart1 => panic!("Shouldn't find part1 visited"),
+//             }
+//         } else {
+//             panic!("should always find tile")
+//         }
+//     } else {
+//         0
+//     }
+// }
+
+fn loops(tiles: &mut HashMap<(usize, usize), Tile>, size: &BoardSize) -> u32 {
     // let (guard_pos, guard_tile) = tiles.iter().find(|(pos, tile)| {
     let (guard_pos, guard_tile) = tiles
         .iter()
@@ -212,32 +274,59 @@ fn loops(tiles: &HashMap<(usize, usize), Tile>, size: &BoardSize, has_branched: 
     };
 
     // let non_branching_loops: u32 = if let Some(pos_in_front) =
-    let non_branching_loops: u32 = if let Some(pos_in_front) =
+    if let Some(pos_in_front) =
         pos_in_direction(guard_pos, &guard_direction, size)
     {
-        let mut next_tiles = tiles.clone();
-        next_tiles.insert(guard_pos, Tile::VisitedPart2(guard_direction.clone()));
+        tiles.insert(guard_pos, Tile::VisitedPart2(guard_direction.clone()));
         if let Some(tile_in_front) = tiles.get(&pos_in_front) {
             match tile_in_front {
                 Tile::Unvisited => {
                     // Set tile in front as guard with same direction
-                    next_tiles.insert(pos_in_front, guard_tile);
-                    loops(&next_tiles, size, has_branched)
+                    tiles.insert(pos_in_front, guard_tile);
+                    loops(tiles, size)
                 }
                 Tile::VisitedPart2(prev_direction) => {
                     if guard_direction == *prev_direction {
                         // Found a loop!
                         return 1;
                     }
-                    next_tiles.insert(pos_in_front, guard_tile);
-                    loops(&next_tiles, size, has_branched)
+                    tiles.insert(pos_in_front, guard_tile);
+                    loops(tiles, size)
                 }
                 Tile::Obstruction => {
                     let turned_direction = guard_direction.turn_right();
-                    if let Some(turned_pos) = pos_in_direction(guard_pos, &turned_direction, &size)
+                    if let Some(turned_next_pos) =
+                        pos_in_direction(guard_pos, &turned_direction, &size)
                     {
-                        next_tiles.insert(turned_pos, Tile::Guard(turned_direction.clone()));
-                        loops(&next_tiles, size, has_branched)
+                        if let Some(turned_next_next_pos) =
+                            pos_in_direction(turned_next_pos, &turned_direction, size)
+                        {
+                            if let Some(turned_next_next_tile) = tiles.get(&turned_next_next_pos) {
+                                match turned_next_next_tile {
+                                    Tile::Obstruction => {
+                                        if let Some(turned_next_tile) = tiles.get(&turned_next_pos)
+                                        {
+                                            match turned_next_tile {
+                                                Tile::VisitedPart2(next_direction)
+                                                    if turned_direction.turn_right()
+                                                        == *next_direction =>
+                                                {
+                                                    // Loop!
+                                                    println!("Obstruction loop found!");
+                                                    return 1;
+                                                }
+                                                _ => (),
+                                            }
+                                        }
+                                    }
+                                    _ => (),
+                                }
+                            }
+                        }
+
+                        // tiles.insert(turned_next_pos, Tile::Guard(guard_direction.clone()));
+                        tiles.insert(turned_next_pos, Tile::Guard(turned_direction.clone()));
+                        loops(tiles, size)
                     } else {
                         // Outside of board
                         0
@@ -253,35 +342,7 @@ fn loops(tiles: &HashMap<(usize, usize), Tile>, size: &BoardSize, has_branched: 
         }
     } else {
         0
-    };
-
-    let branching_loops: u32 = if !has_branched {
-        if let Some(pos_in_front) = pos_in_direction(guard_pos, &guard_direction, size) {
-            if let Some(tile_in_front) = tiles.get(&pos_in_front) {
-                match tile_in_front {
-                    Tile::Obstruction => 0,
-                    _ => {
-                        let mut cloned_tiles = tiles.clone();
-
-                        // Set tile in front as obstruction
-                        cloned_tiles.insert(pos_in_front, Tile::Obstruction);
-                        loops(&cloned_tiles, size, true)
-                    }
-                }
-            } else {
-                // No tile in front, no loops
-                0
-            }
-        } else {
-            // No tile in front, no loops
-            0
-        }
-    } else {
-        // Shouldn't consider loops if already branched
-        0
-    };
-
-    return branching_loops + non_branching_loops;
+    }
 }
 
 pub fn part_one(input: &str) -> Option<u32> {
@@ -312,12 +373,37 @@ pub fn part_one(input: &str) -> Option<u32> {
     Some(count)
 }
 
+// pub fn part_two(input: &str) -> Option<u32> {
+//     let board = Board::parse(input)?;
+//     let size = board.size.clone();
+//     let mut tiles = board.tiles.clone();
+
+//     let loop_count = loops(&mut tiles, &size, false);
+
+//     Some(loop_count)
+// }
+
 pub fn part_two(input: &str) -> Option<u32> {
     let board = Board::parse(input)?;
     let size = board.size.clone();
     let mut tiles = board.tiles.clone();
 
-    let loop_count = loops(&tiles, &size, false);
+    let mut loop_count = 0;
+
+    let variants = board.tiles.iter().filter(|(_, tile)| {
+        match tile {
+            Tile::Unvisited => true,
+            _ => false
+        }
+    }).map(|(pos, _)| pos);
+
+
+    for variant_pos in variants {
+        let mut tile_variant = tiles.clone();
+        tile_variant.insert(*variant_pos, Tile::Obstruction);
+
+        loop_count += loops(&mut tile_variant, &size);
+    }
 
     Some(loop_count)
 }
@@ -336,5 +422,11 @@ mod tests {
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
         assert_eq!(result, Some(6));
+    }
+
+    #[test]
+    fn test_part_two_small_loop() {
+        let result = part_two(&advent_of_code::template::read_file_part("examples", DAY, 1));
+        assert_eq!(result, Some(1));
     }
 }
