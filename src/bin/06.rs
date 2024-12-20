e25460873a6c350b9a3fb7138a2d7791eef3b1e6
Kristian Brudeli <krbrudeli@gmail.com>
2024-12-20 18:12:42 +0100

    WIP day 6 part 2

--- src/bin/06.rs
+++ src/bin/06.rs
@@ -178,14 +178,8 @@ fn step_part_1(tiles: &mut HashMap<(usize, usize), Tile>, size: &BoardSize) -> b
                 }
                 Tile::Obstruction => {
                     let turned_direction = guard_direction.turn_right();
-                    if let Some(turned_pos) = pos_in_direction(guard_pos, &turned_direction, &size)
-                    {
-                        tiles.insert(turned_pos, Tile::Guard(turned_direction.clone()));
-                        return false;
-                    } else {
-                        // Outside of board
-                        return true;
-                    }
+                    tiles.insert(guard_pos, Tile::Guard(turned_direction));
+                    return false;
                 }
                 Tile::Guard(_) => {
                     panic!("should not have more than one guard")
@@ -201,62 +195,48 @@ fn step_part_1(tiles: &mut HashMap<(usize, usize), Tile>, size: &BoardSize) -> b
     }
 }
 
-// fn loops(tiles: &mut HashMap<(usize, usize), Tile>, size: &BoardSize) -> u32 {
-//     // let (guard_pos, guard_tile) = tiles.iter().find(|(pos, tile)| {
-//     let (guard_pos, guard_tile) = tiles
-//         .iter()
-//         .find(|(_, tile)| matches!(tile, Tile::Guard(_)))
-//         .map(|(pos, tile)| (*pos, tile.clone()))
-//         .unwrap();
 
-//     // tiles.insert(guard_pos, Tile::VisitedPart2());
+fn step_part_2(tiles: &mut HashMap<(usize, usize), Tile>, size: &BoardSize) -> bool {
+    // let (guard_pos, guard_tile) = tiles.iter().find(|(pos, tile)| {
+    let (guard_pos, guard_tile) = tiles
+        .iter()
+        .find(|(_, tile)| matches!(tile, Tile::Guard(_)))
+        .map(|(pos, tile)| (*pos, tile.clone()))
+        .unwrap();
 
-//     let guard_direction = match guard_tile.clone() {
-//         Tile::Guard(direction) => direction,
-//         _ => panic!("Guard tile should always be guard"),
-//     };
+    tiles.insert(guard_pos, Tile::VisitedPart1);
 
-//     // let non_branching_loops: u32 = if let Some(pos_in_front) =
-//     if let Some(pos_in_front) = pos_in_direction(guard_pos, &guard_direction, size) {
-//         tiles.insert(guard_pos, Tile::VisitedPart2(guard_direction.clone()));
-//         if let Some(tile_in_front) = tiles.get(&pos_in_front) {
-//             match tile_in_front {
-//                 Tile::Unvisited => {
-//                     // Set tile in front as guard with same direction
-//                     tiles.insert(pos_in_front, guard_tile);
-//                     loops(tiles, size)
-//                 }
-//                 Tile::VisitedPart2(prev_direction) => {
-//                     if guard_direction == *prev_direction {
-//                         // Found a loop!
-//                         return 1;
-//                     }
-//                     tiles.insert(pos_in_front, guard_tile);
-//                     loops(tiles, size)
-//                 }
-//                 Tile::Obstruction => {
-//                     let turned_direction = guard_direction.turn_right();
-//                     if let Some(turned_pos) = pos_in_direction(guard_pos, &turned_direction, &size)
-//                     {
-//                         tiles.insert(turned_pos, Tile::Guard(turned_direction.clone()));
-//                         loops(tiles, size)
-//                     } else {
-//                         // Outside of board
-//                         0
-//                     }
-//                 }
-//                 Tile::Guard(_) => {
-//                     panic!("should not have more than one guard")
-//                 }
-//                 Tile::VisitedPart1 => panic!("Shouldn't find part1 visited"),
-//             }
-//         } else {
-//             panic!("should always find tile")
-//         }
-//     } else {
-//         0
-//     }
-// }
+    let guard_direction = match guard_tile.clone() {
+        Tile::Guard(direction) => direction,
+        _ => panic!("Guard tile should always be guard"),
+    };
+
+    if let Some(pos_in_front) = pos_in_direction(guard_pos, &guard_direction, size) {
+        if let Some(tile_in_front) = tiles.get(&pos_in_front) {
+            match tile_in_front {
+                Tile::Unvisited | Tile::VisitedPart1 => {
+                    // Set tile in front as guard with same direction
+                    tiles.insert(pos_in_front, guard_tile);
+                    return false;
+                }
+                Tile::Obstruction => {
+                    let turned_direction = guard_direction.turn_right();
+                    tiles.insert(guard_pos, Tile::Guard(turned_direction));
+                    return false;
+                }
+                Tile::Guard(_) => {
+                    panic!("should not have more than one guard")
+                }
+                Tile::VisitedPart2(_) => panic!("Should not find these"),
+            }
+        } else {
+            panic!("should always find tile")
+        }
+    } else {
+        println!("Outside of board!");
+        true
+    }
+}
 
 fn loops(tiles: &mut HashMap<(usize, usize), Tile>, size: &BoardSize) -> u32 {
     // let (guard_pos, guard_tile) = tiles.iter().find(|(pos, tile)| {
@@ -358,7 +338,8 @@ pub fn part_one(input: &str) -> Option<u32> {
         // println!("{}", steps);
         // let cloned_tiles = tiles.clone();
         // Board::print(&Board { tiles: cloned_tiles, size });
-        finished = step_part_1(&mut tiles, &size);
+        // finished = step_part_1(&mut tiles, &size);
+        finished = step_part_2(&mut tiles, &size);
         steps += 1;
     }
 
@@ -424,9 +405,9 @@ mod tests {
         assert_eq!(result, Some(6));
     }
 
-    #[test]
-    fn test_part_two_small_loop() {
-        let result = part_two(&advent_of_code::template::read_file_part("examples", DAY, 1));
-        assert_eq!(result, Some(1));
-    }
+    // #[test]
+    // fn test_part_two_small_loop() {
+    //     let result = part_two(&advent_of_code::template::read_file_part("examples", DAY, 1));
+    //     assert_eq!(result, Some(1));
+    // }
 }
